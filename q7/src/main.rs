365dc09f1746547e012c1b31c5190a42af0b0911
Era Toriele <fafadaw1234@gmail.com>
2026-01-04 21:50:43 +0300

    completed q7

--- q7/src/main.rs
+++ q7/src/main.rs
@@ -3,20 +3,20 @@ use std::fs;
 #[derive(Clone)]
 enum PointType {
     Start,
-    Beam,
+    Beam(u64),
     Splitter,
     Space,
 }
 impl std::fmt::Debug for PointType {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let printable = match self {
-            PointType::Start => &"Start",
-            PointType::Beam => &"Beam",
-            PointType::Splitter => &"Splitter",
-            PointType::Space => &"Space",
+        let printable: String = match self {
+            PointType::Start => "Start".to_string(),
+            PointType::Beam(x) => format!("Beam, {x}"),
+            PointType::Splitter => "Splitter".to_string(),
+            PointType::Space => "Space".to_string(),
         };
         f.debug_struct("PointType")
-            .field("type", printable)
+            .field("type", &printable)
             .finish()
     }
 }
@@ -32,13 +32,20 @@ impl Point {
 
 fn print_line(line: &[PointType]) {
     for point in line {
-        let val = match point {
-            PointType::Space => ".",
-            PointType::Start => "S",
-            PointType::Splitter => "^",
-            PointType::Beam => "|",
+        // let val = match point {
+        //     PointType::Space => ".",
+        //     PointType::Start => "S",
+        //     PointType::Splitter => "^",
+        //     PointType::Beam(x) => "|",
+        // };
+        // print!("{val}");
+        let val: String = match point {
+            PointType::Start => "S".to_string(),
+            PointType::Beam(x) => format!("{x}"),
+            PointType::Splitter => "^".to_string(),
+            PointType::Space => ".".to_string(),
         };
-        print!("{val}");
+        print!("-{val}-");
     }
     println!();
 }
@@ -48,13 +55,27 @@ fn move_beam_down(map: &[Vec<PointType>], level: usize) -> (Vec<Vec<PointType>>,
     let mut split_count = 0u64;
 
     for (index, point) in return_map[level].clone().iter().enumerate() {
-        if let PointType::Beam = point {
+        if let PointType::Beam(c) = point {
             if let PointType::Splitter = return_map[level + 1][index] {
                 split_count += 1;
-                return_map[level + 1][index - 1] = PointType::Beam;
-                return_map[level + 1][index + 1] = PointType::Beam;
+                return_map[level + 1][index - 1] = match return_map[level + 1][index - 1] {
+                    PointType::Beam(x) => PointType::Beam(x + c),
+                    _ => PointType::Beam(*c),
+                };
+                return_map[level + 1][index + 1] = match return_map[level + 1][index + 1] {
+                    PointType::Beam(x) => PointType::Beam(x + c),
+                    _ => PointType::Beam(*c),
+                };
             } else if let PointType::Space = return_map[level + 1][index] {
-                return_map[level + 1][index] = PointType::Beam;
+                return_map[level + 1][index] = match return_map[level + 1][index] {
+                    PointType::Beam(x) => PointType::Beam(x + c),
+                    _ => PointType::Beam(*c),
+                };
+            } else if let PointType::Beam(_) = return_map[level + 1][index] {
+                return_map[level + 1][index] = match return_map[level + 1][index] {
+                    PointType::Beam(x) => PointType::Beam(x + c),
+                    _ => PointType::Beam(*c),
+                };
             }
         }
     }
@@ -62,6 +83,25 @@ fn move_beam_down(map: &[Vec<PointType>], level: usize) -> (Vec<Vec<PointType>>,
 
     (return_map, split_count)
 }
+
+// fn part2(map: &[Vec<PointType>]) -> u64 {
+//     let mut total_paths = 0u64;
+//
+//     for (level, line) in map.iter().enumerate() {
+//         if level != 1 && level % 2 == 1 {
+//             let paths = line.iter().fold(0, |acc, x| {
+//                 acc + match x {
+//                     PointType::Beam => 1,
+//                     _ => 0,
+//                 }
+//             });
+//             print_line(line);
+//             println!("{level}, {paths}");
+//             total_paths += paths;
+//         }
+//     }
+//     total_paths
+// }
 
 fn main() {
     // let file_path = "./input/example";
@@ -85,7 +125,7 @@ fn main() {
         }
     }
     let start_point = start_point; // mut the value
-    map[start_point.x + 1][start_point.y] = PointType::Beam; // the first beam
+    map[start_point.x + 1][start_point.y] = PointType::Beam(1); // the first beam
 
     let mut total_splits = 0u64;
     // The last line is empty
@@ -95,5 +135,12 @@ fn main() {
         total_splits += splits;
     }
 
-    println!("{total_splits}");
+    println!("part1: {total_splits}");
+    // println!("part2: {}", part2(&map));
+    // print_line(map.last().unwrap());
+    let part2 = map.last().unwrap().iter().fold(0, |acc, x| match x {
+        PointType::Beam(x) => acc + x,
+        _ => acc,
+    });
+    println!("{part2}");
 }
